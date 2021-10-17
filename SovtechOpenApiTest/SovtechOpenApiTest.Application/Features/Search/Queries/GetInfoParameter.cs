e38dc5332a9fa9ce20ec8b48eaf330b03c513e09
Russel Makaringi <rhlayi@gmail.com>
2021-10-18 00:53:19 +0200

    More endpoints

---
+++ SovtechOpenApiTest/SovtechOpenApiTest.Application/Features/Search/Queries/GetInfoParameter.cs
@@ -0,0 +1,13 @@
+﻿using SovtechOpenApiTest.Application.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SovtechOpenApiTest.Application.Features.Chuck.Queries.GetAllCategories
+{
+    
+    public class GetInfoParameter : RequestParameter
+    {
+
+    }
+}
---
+++ SovtechOpenApiTest/SovtechOpenApiTest.Application/Features/Search/Queries/GetInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SovtechOpenApiTest.Application.Features.Chuck.Queries.GetCategoryDetails
+{
+    public class GetInfoViewModel
+    {
+        public string  Name { get; set; }
+    }
+}
